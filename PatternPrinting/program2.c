b25f35eed91d231179a6246c97d30c2d00dcb882
Jothshana <jothshana.cs22@bitsathy.ac.in>
2024-03-13 19:10:13 +0530

    pattern printing

--- PatternPrinting/program2.c
+++ PatternPrinting/program2.c
@@ -5,7 +5,7 @@ int main()
     scanf("%d", &num);
     for (int i = 0; i < num; i++)
     {
-        for (int j = 0; j < num; j++)
+        for (int j = 0; j <= i; j++)
         {
             printf("* ");
         }
@@ -15,13 +15,12 @@ int main()
 }
 
 /*
-
 INPUT:5
 OUTPUT:
-* * * * *
-* * * * *
-* * * * *
-* * * * *
+* 
+* * 
+* * * 
+* * * * 
 * * * * *
 
 */
