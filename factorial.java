122394620144f4368a24ae2653f65a7dadde5c26
JOTHSHANA <jothshana123@gmail.com>
2024-03-23 15:25:45 +0530

    factorial solution

---
+++ factorial.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class factorial
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.factorial(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution{
+    static long factorial(int N){
+        if (N == 0) {
+      return 1;
+    } else {
+      return N * factorial(N - 1);
+    }
+    }
+}
+
+
+/**
+
+INPUT:
+2
+5
+4
+OUTPUT:
+120
+24
+
+ */
