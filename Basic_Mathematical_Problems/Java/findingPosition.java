a93038503f9e901bb6b99c4011f8faf384bac57a
JOTHSHANA <jothshana123@gmail.com>
2024-04-30 08:40:54 +0530

    Finding position

---
+++ Basic_Mathematical_Problems/Java/findingPosition.java
@@ -0,0 +1,58 @@
+// SOLVED IN GEEKS FOR GEEKS
+// PROBLEM NAME : Finding Position
+
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+class findingPosition {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            long n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+
+            System.out.println(ob.nthPosition(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    static long nthPosition(long n){
+        int i=1;
+        int prev=0;
+        while(n>i)
+        {
+            
+            prev=i;
+            i=i*2;
+        }
+        if(i==n)
+        {
+            return i;
+        }
+        return prev;
+    }
+}
+
+
+
+
+
+/*
+
+INPUT:
+2
+5
+9
+OUTPUT:
+4
+8
+
+*/
+
