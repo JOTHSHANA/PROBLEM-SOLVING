baf67417be83f09ae046507c118cb64a5a7bfe66
JOTHSHANA <jothshana123@gmail.com>
2024-05-06 22:15:39 +0530

    added one more problem

---
+++ Basic_Mathematical_Problems/C/sumAndProduct.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+    
+    int product = 1;
+    int sum = 0;
+    for(int i = 0; i < n; i++){
+        product*=arr[i];
+        sum+=arr[i];
+    }
+    
+    printf("%d, %d", sum, product);
+}
