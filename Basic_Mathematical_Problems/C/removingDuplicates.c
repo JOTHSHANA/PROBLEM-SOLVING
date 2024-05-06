a4105f2aa8e91803e61bb2936cd5fe512516d36e
JOTHSHANA <jothshana123@gmail.com>
2024-05-06 23:19:35 +0530

    removing Duplicates

---
+++ Basic_Mathematical_Problems/C/removingDuplicates.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] == arr[i])
+            {
+                for (int k = j; k < n - 1; k++)
+                {
+                    arr[k] = arr[k + 1];
+                }
+                n--;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d", arr[i]);
+    }
+
+    return 0;
+}
