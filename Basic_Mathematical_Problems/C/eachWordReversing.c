7dbf5adb583fb6484cf27ea722474b182987d2a8
JOTHSHANA <jothshana123@gmail.com>
2024-05-11 09:36:08 +0530

    reversing each word in a sentence

---
+++ Basic_Mathematical_Problems/C/eachWordReversing.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100];
+    fgets(str, sizeof(str), stdin);
+    if (str[strlen(str) - 1] == '\n')
+    {
+        str[strlen(str) - 1] = '\0';
+    }
+
+    int length = strlen(str);
+    int start = 0;
+    int end;
+    for (int i = 0; i <= length; i++)
+    {
+        if (str[i] == ' ' || str[i] == '\0')
+        {
+            end = i - 1;
+            while (start < end)
+            {
+                int temp = str[start];
+                str[start] = str[end];
+                str[end] = temp;
+                start++;
+                end--;
+            }
+            start = i + 1;
+        }
+    }
+
+    for (int i = length - 1; i >= 0; i--)
+    {
+        printf("%c", str[i]);
+    }
+
+    return 0;
+}
